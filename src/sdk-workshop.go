83465eb6d5b3b8ef4850a4b87005aa1da8348e6d
Juan Hernandez <jhernandez@newrelic.com>
2018-05-04 16:28:38 +0200

    Quering on real http mocks

--- src/sdk-workshop.go
+++ src/sdk-workshop.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+
 	sdkArgs "github.com/newrelic/infra-integrations-sdk/args"
 	"github.com/newrelic/infra-integrations-sdk/data/event"
 	"github.com/newrelic/infra-integrations-sdk/data/metric"
@@ -18,15 +23,17 @@ const (
 
 var (
 	args argumentList
-	endpointA = "http://localhost:8881"
-	endpointB = "http://localhost:8882"
+	//endpointA = "http://localhost:8081"
+	//endpointB = "http://localhost:8082"
+	endpointA = "http://www.mocky.io/v2/5aec67303200006700fa48fb"
+	endpointB = "http://www.mocky.io/v2/5aec674b3200004a00fa48fc"
 )
 
 type serverStatus struct {
-	statusCode int
-	apiVersion string
-	latencyMs int
-	error string
+	StatusCode int    `json:"status"`
+	ApiVersion string `json:"version"`
+	LatencyMs  int64  `json:"-"`
+	Error      string `json:"error"`
 }
 
 func main() {
@@ -55,10 +62,13 @@ func main() {
 }
 
 func monitorizeWebServer(e *integration.Entity, endpoint string) error {
-	s := queryServer(endpoint)
+	s, err := queryServer(endpoint)
+	if err != nil {
+		return err
+	}
 
 	// Add Inventory
-	e.SetInventoryItem("api", "version", s.apiVersion)
+	e.SetInventoryItem("api", "version", s.ApiVersion)
 
 	// Add Metrics
 	set, err := e.NewMetricSet("status")
@@ -66,29 +76,40 @@ func monitorizeWebServer(e *integration.Entity, endpoint string) error {
 		return err
 	}
 
-	set.SetMetric("status", s.statusCode, metric.GAUGE)
-	set.SetMetric("latency", s.latencyMs, metric.DELTA)
+	set.SetMetric("status", s.StatusCode, metric.GAUGE)
+	set.SetMetric("latency", s.LatencyMs, metric.GAUGE)
 
-	if s.statusCode >= 500 {
+	if s.StatusCode >= 500 {
 		// Add Event
-		e.AddEvent(event.New(s.error, "error"))
+		e.AddEvent(event.New(s.Error, "error"))
 	}
 
 	return nil
 }
 
-func queryServer(endpoint string) serverStatus {
-	status := 200
-	version := "1.0.0"
-	errorMsg := ""
-	latency := 150
-
-	return serverStatus{
-		statusCode: status,
-		apiVersion: version,
-		latencyMs: latency,
-		error: errorMsg,
+func queryServer(endpoint string) (s serverStatus, err error) {
+	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return
 	}
+
+	start := time.Now()
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	latency := time.Now().Sub(start)
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(body, &s)
+
+	s.LatencyMs = latency.Nanoseconds() / 1000000
+
+	return
 }
 
 func panicOnErr(err error) {
